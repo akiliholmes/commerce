210df28459889b0e8f4d7ce818642e6053d7192f
Admin <akili.holmes@gmail.com>
2021-01-15 11:50:29 -0800

    finished models.py

--- auctions/models.py
+++ auctions/models.py
@@ -13,15 +13,16 @@ class Category(models.Model):
         return f"{self.category}"
 
 class Listing(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="author_list")
     title = models.CharField(max_length=64)
     image = models.ImageField(upload_to="images/")
     published = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_list")
     description = models.TextField(max_length=256)
     currentBid = models.FloatField(blank=True, null=True)
     startingBid = models.FloatField()
     ended = models.BooleanField(default=False)
-    watchlist = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, )
+    watchlist = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="watch_list")
 
     def __str__(self):
         return f"{self.title} {self.currentBid}"
@@ -30,17 +31,17 @@ class Listing(models.Model):
 class Bid(models.Model):
     bidder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    previousBid = models.IntegerField()
-    currentBid = models.IntegerField()
+    date = models.DateTimeField(default=timezone.now)
     newBid = models.IntegerField()
 
     def __str__(self):
-        return f"{self.currentBid}"
+        return f"{self.newBid}"
 
 class Comment(models.Model):
     commenter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     published = models.DateTimeField(default=timezone.now)
     message = models.TextField(max_length=256)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_review")
 
     def __str__(self):
         return f"{self.message}"
